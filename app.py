41c6fec4358f93691b17249148fb269f11e40b33
Shyambhuva4 <91263012+Shyambhuva4@users.noreply.github.com>
2023-05-07 10:41:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ import pandas as pd
 from io import BytesIO
 import time 
 
-st.title("Food Recommendation System")
+st.title("Time series Visualizer")
 
     
-st.text("Let us help you with food")
+st.text("Let us help you with data ☺")
 
 
 
-##### IMPLEMENTING RECOMMENDER ######
+##### IMPLEMENTING Visualizer ######
 
 def print_on_site():
      try:
@@ -28,27 +28,42 @@ def print_on_site():
           return 0
      return consumer
 
+bruh = st.checkbox("Choose your Device")
 
 consumer = print_on_site()
 serialized_df_response = next(consumer).value
 
         # Convert the serialized DataFrame back to a Pandas DataFrame
 df_response = pd.read_csv(BytesIO(serialized_df_response.encode()))
-row = df_response.iloc[0]
+
 
 # Display the values of the row horizontally in Streamlit
 
-table = pd.DataFrame(columns=df_response.columns)
+# df_response["ts"] = pd.to_datetime(df_response["ts"], unit="s")
 
-# Display the empty table
-st.table(table)
-for i, row in df_response.iterrows():
-    # Wait for 1 second to simulate streaming data
-    time.sleep(1)
+# Set the timestamp column as the index of the DataFrame
+df_response.set_index("ts", inplace=True)
+devices=df_response["device"].unique().tolist()
+if bruh==True:
+ finallist = st.selectbox("Our Choices",devices)
 
-    # Append the row to the table
-    table = table.append(row, ignore_index=True)
+# Resample the data to a fixed frequency (e.g. every 5 minutes) and forward fill missing values
+filtered_df = df_response[df_response["device"] == finallist]
 
-    # Display the updated table
-    st.experimental_set_query_params(row=i)
-    st.table(table)
+bruh2 = st.checkbox("Fasinates")
+if bruh2:
+# Plot the "co", "humidity", and "temp" columns for the selected device using Streamlit's line_chart function
+     st.line_chart(filtered_df[["co"]].tail(100))
+     st.line_chart(filtered_df[["humidity"]].tail(100))
+     st.line_chart(filtered_df[["lpg"]].tail(100))
+     st.line_chart(filtered_df[["smoke"]].tail(100))
+     st.line_chart(filtered_df[["temp"]].tail(100))
+# Plot the data using Streamlit's line_chart function
+
+# for device, group in grouped_df:
+#     st.line_chart(group["co"].tail(100))
+#     st.line_chart(group['humidity'].tail(100))
+#     st.line_chart(group['lpg'].tail(100))
+#     st.line_chart(group['smoke'].tail(100))
+#     st.line_chart(group['temp'].tail(100))
+
