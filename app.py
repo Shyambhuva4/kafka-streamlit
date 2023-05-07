137d815002ef42043f78aae17929e774b53f6d50
Shyambhuva4 <91263012+Shyambhuva4@users.noreply.github.com>
2023-05-07 11:50:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,10 +54,15 @@ bruh2 = st.checkbox("Fasinates")
 if bruh2:
 # Plot the "co", "humidity", and "temp" columns for the selected device using Streamlit's line_chart function
      st.line_chart(filtered_df[["co"]].tail(100))
+     st.write("maximum value of CO emission is :"+ str(max(filtered_df["co"])))
      st.line_chart(filtered_df[["humidity"]].tail(100))
+     st.write("maximum humidity is :"+ str(max(filtered_df["humidity"])))
      st.line_chart(filtered_df[["lpg"]].tail(100))
+     st.write("maximum lpg is :"+ str(max(filtered_df["lpg"])))
      st.line_chart(filtered_df[["smoke"]].tail(100))
+     st.write("maximum value of smoke emission is :"+ str(max(filtered_df["smoke"])))
      st.line_chart(filtered_df[["temp"]].tail(100))
+     st.write("maximum value of temperature is :"+ str(max(filtered_df["temp"])))
 # Plot the data using Streamlit's line_chart function
 
 # for device, group in grouped_df:
@@ -66,4 +71,3 @@ if bruh2:
 #     st.line_chart(group['lpg'].tail(100))
 #     st.line_chart(group['smoke'].tail(100))
 #     st.line_chart(group['temp'].tail(100))
-
